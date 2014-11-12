2bc8d611537974d6a00727e9fd9e1033e65cdb92
apstrinka <apstrinka@gmail.com>
2014-11-11 16:54:34 -0800

    Asteroids get smaller when hit

--- asteroid.js
+++ asteroid.js
@@ -1,7 +1,9 @@
-var screenWidth = $(window).width();
-var screenHeight = $(window).height();
-$('.ship').css('top',screenHeight/2-10 + 'px').css('left',screenWidth/2-10 + 'px');
+var screenWidth = $(window).innerWidth();
+var screenHeight = $(window).innerHeight();
+$('.ship').css('top',screenHeight/2-15 + 'px').css('left',screenWidth/2-15 + 'px');
+$('.ship').children().css('top', 5).css('left', 5);
 var going = false;
+var score = 0;
 var interval;
 var ship;
 var bullets = [];
@@ -41,7 +43,8 @@ var main = function() {
 			}
 			break;
 		case 32: //space
-			if (going){
+			if (going && ship.coolDown == 0){
+				ship.coolDown = 5;
 				var xVel = ship.xVel + 10*Math.cos(ship.ang*Math.PI/180);
 				var yVel = ship.yVel + 10*Math.sin(ship.ang*Math.PI/180);
 				
@@ -53,6 +56,7 @@ var main = function() {
 }
 
 var restart = function(){
+	score = 0;
 	ship = new Ship(screenWidth/2, screenHeight/2, 0, 0);
 	bullets = [];
 	$('.bullet').remove();
@@ -63,7 +67,7 @@ var restart = function(){
 	var xVel = Math.floor(Math.random()*20 - 10);
 	var yVel = Math.floor(Math.random()*20 - 10);
 	var rot = Math.floor(Math.random()*20 - 10);
-	asteroids.push(new Asteroid(xPos, yPos, xVel, yVel, rot));
+	asteroids.push(new Asteroid(xPos, yPos, xVel, yVel, rot, 40));
 }
 
 var update = function(){
@@ -100,6 +104,8 @@ var update = function(){
 		var t = asteroids[i];
 		t.draw();
 	}
+	
+	$('.score').text(score);
 }
 
 var indexOfOverlappingAsteroid = function(thing){
@@ -119,50 +125,61 @@ var removeBullet = function(index){
 }
 
 var breakAsteroid = function(index){
+	score++;
 	var a = asteroids[index];
 	$('#'+a.id).remove();
 	asteroids.splice(index, 1);
-	var dx = Math.floor(Math.random()*20-10);
-	var dy = Math.floor(Math.random()*20-10);
-	var dr = Math.floor(Math.random()*20-10);
-	asteroids.push(new Asteroid(a.xPos, a.yPos, a.xVel+dx, a.yVel+dy, a.rot+dr));
-	asteroids.push(new Asteroid(a.xPos, a.yPos, a.xVel-dx, a.yVel-dy, a.rot-dr));
+	if (a.radius > 5){
+		var dx = Math.floor(Math.random()*20-10);
+		var dy = Math.floor(Math.random()*20-10);
+		var dr = Math.floor(Math.random()*20-10);
+		asteroids.push(new Asteroid(a.xPos, a.yPos, a.xVel+dx, a.yVel+dy, a.rot+dr, a.radius/2));
+		asteroids.push(new Asteroid(a.xPos, a.yPos, a.xVel-dx, a.yVel-dy, a.rot-dr, a.radius/2));
+	}
 }
 
 function Ship(xPos, yPos, xVel, yVel){
-	this.radius = 7;
 	this.xPos = xPos;
 	this.yPos = yPos;
 	this.xVel = xVel;
 	this.yVel = yVel;
 	this.ang = 0;
+	this.radius = 7;
+	this.coolDown = 0;
 	
 	this.update = function(){
 		this.xPos = (this.xPos + this.xVel + screenWidth) % screenWidth;
 		this.yPos = (this.yPos + this.yVel + screenHeight) % screenHeight;
+		if (this.coolDown > 0)
+			this.coolDown--;
 	}
 	
 	this.draw = function(){
-		var top = (this.yPos-10) + 'px';
-		var left = (this.xPos-10) + 'px';
+		var top = (this.yPos-15) + 'px';
+		var left = (this.xPos-15) + 'px';
+		var height = 15 + Math.min(15, screenHeight-this.yPos);
+		var width = 15 + Math.min(15, screenWidth-this.xPos);
 		var rotate = 'rotate(' + this.ang + 'deg)';
-		$('.ship').css('top',top).css('left',left).css('-webkit-transform',rotate).css('-moz-transform',rotate).css('-o-transform',rotate).css('-ms-transform',rotate).css('transform',rotate);
+		var div = $('.ship').css('top',top).css('left',left).css('height', height).css('width', width);
+		div.children().css('-webkit-transform',rotate).css('-moz-transform',rotate).css('-o-transform',rotate).css('-ms-transform',rotate).css('transform',rotate);
 	}
 }
 
 var bulletNum = 0;
 
 function Bullet(xPos, yPos, xVel, yVel){
-	this.radius = 1;
-	this.decay = 100;
 	this.xPos = xPos;
 	this.yPos = yPos;
 	this.xVel = xVel;
 	this.yVel = yVel;
+	this.radius = 1;
+	this.decay = 100;
 	bulletNum++;
 	this.id = 'bullet' + bulletNum;
-	var img = $('<img/>').addClass('bullet').attr('id', this.id).attr('src', 'bullet.png').css('top', yPos+'px').css('left', xPos+'px');
-	$('body').append(img);
+	var div = $('<div>').addClass('bullet').attr('id', this.id).css('top', (yPos-3)+'px').css('left', (xPos-3)+'px');
+	var img = $('<img/>').attr('src', 'bullet.png');
+	div.append(img);
+	$('body').append(div);
 	
 	this.update = function(){
 		this.decay = this.decay - 1;
@@ -173,25 +190,29 @@ function Bullet(xPos, yPos, xVel, yVel){
 	this.draw = function(){
 		var top = (this.yPos-3) + 'px';
 		var left = (this.xPos-3) + 'px';
-		var img = $('#'+this.id).css('top',top).css('left',left);
+		var height = 3 + Math.min(3, screenHeight-this.yPos);
+		var width = 3 + Math.min(3, screenWidth-this.xPos);
+		var div = $('#'+this.id).css('top',top).css('left',left).css('height', height).css('width', width);
 	}
 }
 
 var asteroidNum = 0;
 
-function Asteroid(xPos, yPos, xVel, yVel, rot){
-	this.radius = 40;
+function Asteroid(xPos, yPos, xVel, yVel, rot, radius){
 	this.xPos = xPos;
 	this.yPos = yPos;
 	this.xVel = xVel;
 	this.yVel = yVel;
 	this.ang = Math.floor(Math.random()*360);
 	this.rot = rot;
+	this.radius = radius;
 	asteroidNum++;
 	this.id = 'asteroid' + asteroidNum;
 	var imgNum = Math.floor(Math.random()*2+1);
-	var img = $('<img/>').addClass('asteroid').attr('id', this.id).attr('src', 'asteroid'+imgNum+'.png').css('top', yPos+'px').css('left', xPos+'px');
-	$('body').append(img);
+	var div = $('<div>').addClass('asteroid').attr('id', this.id).css('height', 3*this.radius).css('width', 3*this.radius).css('top', yPos+'px').css('left', xPos+'px');
+	var img = $('<img/>').attr('src', 'asteroid'+imgNum+'.png').css('height', 2*this.radius).css('width', 2*this.radius).css('top', .5*this.radius+'px').css('left', .5*this.radius+'px');
+	div.append(img);
+	$('body').append(div);
 	
 	this.update = function(){
 		this.xPos = (this.xPos + this.xVel + screenWidth) % screenWidth;
@@ -200,10 +221,13 @@ function Asteroid(xPos, yPos, xVel, yVel, rot){
 	}
 	
 	this.draw = function(){
-		var top = (this.yPos-40) + 'px';
-		var left = (this.xPos-40) + 'px';
+		var top = (this.yPos-1.5*this.radius) + 'px';
+		var left = (this.xPos-1.5*this.radius) + 'px';
+		var height = 1.5*this.radius + Math.min(1.5*this.radius, screenHeight-this.yPos);
+		var width = 1.5*this.radius + Math.min(1.5*this.radius, screenWidth-this.xPos);
 		var rotate = 'rotate(' + this.ang + 'deg)';
-		var img = $('#'+this.id).css('top',top).css('left',left).css('-webkit-transform',rotate).css('-moz-transform',rotate).css('-o-transform',rotate).css('-ms-transform',rotate).css('transform',rotate);
+		var div = $('#'+this.id).css('top',top).css('left',left).css('height', height).css('width', width);
+		div.children().css('-webkit-transform',rotate).css('-moz-transform',rotate).css('-o-transform',rotate).css('-ms-transform',rotate).css('transform',rotate);
 	}
 	
 }
